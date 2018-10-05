fd8628c7279f5efa5031c09e49162f61c84866f1
wkr1u18 <wkr1u18@soton.ac.uk>
2018-10-05 19:47:33 +0100

    reading HTML from URL of website of person with given id

--- src/SpaceCadetOne.java
+++ src/SpaceCadetOne.java
@@ -1,5 +1,24 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.*;
+import java.io.*;
+
 public class SpaceCadetOne {
-    public static void main (String[] args){
-        System.out.println("Hello world\n");
+    public static void main (String[] args) throws Exception {
+        System.out.println("Enter the university id:");
+        BufferedReader textInput = new BufferedReader(new InputStreamReader(System.in));
+        String textId = textInput.readLine();
+        String textUrl = "https://www.ecs.soton.ac.uk/people/" + textId;
+
+        URL personUrl = new URL(textUrl);
+        BufferedReader urlInput = new BufferedReader(new InputStreamReader(personUrl.openStream()));
+        String inputLine;
+        while((inputLine = urlInput.readLine())!=null) {
+            System.out.println(inputLine);
+        }
+
+
+        urlInput.close();
+        textInput.close();
     }
 }
