862cd8832b125f08ff0b402e0a744b206f67266a
wkr1u18 <wkr1u18@soton.ac.uk>
2018-10-06 20:40:33 +0100

    Program meets given specification

--- src/SpaceCadetOne.java
+++ src/SpaceCadetOne.java
@@ -4,7 +4,9 @@ import java.net.*;
 import java.io.*;
 
 public class SpaceCadetOne {
+
     public static void main (String[] args) throws Exception {
+
         System.out.println("Enter the university id:");
         BufferedReader textInput = new BufferedReader(new InputStreamReader(System.in));
         String textId = textInput.readLine();
@@ -13,9 +15,18 @@ public class SpaceCadetOne {
         URL personUrl = new URL(textUrl);
         BufferedReader urlInput = new BufferedReader(new InputStreamReader(personUrl.openStream()));
         String inputLine;
+        String searchedPattern = new String("property=\"name\">");
+        int startPosition = -1, endPosition=-1;
         while((inputLine = urlInput.readLine())!=null) {
-            System.out.println(inputLine);
+            startPosition = inputLine.indexOf(searchedPattern);
+            if (startPosition>0) {
+                endPosition = inputLine.indexOf('<', startPosition);
+                System.out.println(inputLine.substring(startPosition+searchedPattern.length(),endPosition));
+                System.exit(0);
+            }
         }
+        System.out.println("Could not find this person in ecs database");
+        System.exit(0);
 
 
         urlInput.close();
